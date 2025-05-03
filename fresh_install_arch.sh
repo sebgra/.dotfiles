96f8fa16ea256d07f4d3756ba0fe8537074411d0
sebgra <sebastiengradit@hotmail.com>
2025-05-03 21:34:37 +0200

    Corrected installation script

--- fresh_install_arch.sh
+++ fresh_install_arch.sh
@@ -1,40 +1,38 @@
 # Global update
-sudo pacman -Syy;
+sudo pacman -Syy
 
 # Install utilities
-sudo pacman -Sy keepassxc;
-sudo pacman -Sy firefox;
-sudo pacman -Sy git;
-sudo pacman -Sy code;
-sudo curl https://sh.rustup.rs -sSf | sh;
-
+sudo pacman -Sy keepassxc
+sudo pacman -Sy firefox
+sudo pacman -Sy git
+sudo pacman -Sy code
+sudo curl https://sh.rustup.rs -sSf | sh
 
 # Config git account
-ssh-keygen -o -t rsa -C "sebastiengradit@hotmail.com"; # Copy to git
-git config --global user.email sebastiengradit@hotmail.com && git config --global user.name sebgra;
+ssh-keygen -o -t rsa -C "sebastiengradit@hotmail.com" # Copy to git
+git config --global user.email sebastiengradit@hotmail.com && git config --global user.name sebgra
 
 # Clone dotfiles
-git clone git@github.com:sebgra/.dotfiles.git;
+git clone git@github.com:sebgra/.dotfiles.git
 
 # Zsh config
 ## Install zsh
-sudo pacman -Sy zsh;
+sudo pacman -Sy zsh
 
 ## Change bash for zsh
-chsh -s $(which zsh);
+chsh -s $(which zsh)
 
 #Oh-my-zsh part
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # ## reboot to apply
 # reboot;
 
 # Install ghostty
-sudo pacman -Sy ghostty;
+sudo pacman -Sy ghostty
 
 # Install bat
-sudo pacman -Sy bat;
+sudo pacman -Sy bat
 ## Customise bat
 
 mkdir -p "$(bat --config-dir)/themes"
@@ -49,50 +47,50 @@ touch "$(bat --config-file)"
 # Add catpuccin to batcat config
 
 # Install antigen
-git clone https://github.com/zsh-users/antigen.git;
+git clone https://github.com/zsh-users/antigen.git
 cd antigen && source antigen.zsh
-antigen selfupdate;
-antigen update;
-
+antigen selfupdate
+antigen update
 
 # Change theme for zsh
-cd ~/.dotfiles && make && source ~/.zshrc;
+cd ~/.dotfiles && make && source ~/.zshrc
 
 ## Install cargoo first
-curl https://sh.rustup.rs -sSf | sh;
+curl https://sh.rustup.rs -sSf | sh
 
 # Install zoxide (cd)
-curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh;
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
 # Install starship
 ## Install
-curl -sS https://starship.rs/install.sh | sh;
+curl -sS https://starship.rs/install.sh | sh
 
 ## Get nerd font and install
-mkdir ~/.local/share/fonts && ~/.local/share/fonts/;
+mkdir ~/.local/share/fonts
+cd ~/.local/share/fonts/
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip && unzip CascadiaCode.zip;
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip
+unzip CascadiaCode.zip
 
 ## Source fonts
-fc-cache -f -v;
+fc-cache -f -v
 
 # Install eza
-cargo install eza;
+cargo install eza
 
 #Install fzf
-sudo pacman -S fzf;
+sudo pacman -S fzf
 
 # Install fd
 # apt install fd-find;
 
-
 # Install tmux
 # Clone and install tmux
-sudo pacman -Sy tmux;
+sudo pacman -Sy tmux
 
 ## Install tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
-tmux source ~/.tmux.conf;
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
 
 # # Install bat
 # sudo pacman -Sy bat;
@@ -110,75 +108,70 @@ tmux source ~/.tmux.conf;
 # # Add catpuccin to batcat config
 # echo '--theme="Catppuccin Frappe"' >> "$(bat --config-file)"
 
-#Install Tabiew 
+#Install Tabiew
 ## usage on csv tw <path_to_tsv(s)> --separator $'\;' --theme monokai
-cargo install tabiew;
+cargo install tabiew
 
 # Install otree
-cargo install --git https://github.com/fioncat/otree;
+cargo install --git https://github.com/fioncat/otree
 
 # Install serpl
 ## Install ripgrep dependency
-sudo pacman -Sy ripgrep;
-cargo install serpl;
+sudo pacman -Sy ripgrep
+cargo install serpl
 
 # Install airmux
-cargo install airmux;
+cargo install airmux
 
 # Install lazygit
 
-sudo pacman -Sy lazygit;
-
+sudo pacman -Sy lazygit
 
 # Install mambaforge
 ## Make sure not replacing init which is already in .zshrc
-~;
+~
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
 
-
 # Nvim setup
-sudo pacman -Sy neovim;
-
+sudo pacman -Sy neovim
 
 ### Lazygit
 # required
-mv ~/.config/nvim{,.bak};
+mv ~/.config/nvim{,.bak}
 
 # optional but recommended
-mv ~/.local/share/nvim{,.bak};
-mv ~/.local/state/nvim{,.bak};
-mv ~/.cache/nvim{,.bak};
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim;
-rm -rf ~/.config/nvim/.git;
-nvim;
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+nvim
 
 ### Luarocks
 
-sudo pacman -S luarocks;
-luarocks --version;
+sudo pacman -S luarocks
+luarocks --version
 
 # Install fd-find
-cargo install fd-find;
+cargo install fd-find
 
 # Install ghostty
-sudo pacman -Sy ghostty;
+sudo pacman -Sy ghostty
 
 # Nvidia drivers
 ## check harware
-lspci | grep -E "VGA|3D";
+lspci | grep -E "VGA|3D"
 
 # System update
-sudo pacman -Syu --noconfirm;
+sudo pacman -Syu --noconfirm
 
 # Required packages
-sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server-devel;
+sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server-devel
 
 # Build config
-sudo mkinitcpio -P;
-sudo nvidia-xconfig;
+sudo mkinitcpio -P
+sudo nvidia-xconfig
 
-reboot;
-
-
+reboot
